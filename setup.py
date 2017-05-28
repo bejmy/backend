482c278315489ff537e94e08842d476d8b1db0e4
Rafał Selewońko <rafal@selewonko.com>
2017-05-28 14:33:15 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='bejmy',
+    version='0.1',
+    description='Home Money Management',
+    long_description='Home Money Management',
+    author='Rafał Selewońko',
+    author_email='rafal@selewonko.com',
+    maintainer='Rafał Selewońko',
+    maintainer_email='rafal@selewonko.com',
+    url='https://github.org/bejmy/backend/',
+    download_url='https://github.com/bejmy/backend/archive/v0.1.zip',
+    packages=[
+        'bejmy'
+    ],
+)
