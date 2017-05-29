ea38b9962a4a837d827316596734961b20172077
Rafał Selewońko <rafal@selewonko.com>
2017-05-29 23:58:59 +0200

    Transactions with labels.

--- bejmy/labels/signals.py
+++ bejmy/labels/signals.py
@@ -2,17 +2,18 @@ from . import initial
 from .models import Label
 
 
+def _create_labels(tree, user, transaction_type, parent=None):
+    for name, children in tree:
+        label = Label(
+            parent=parent,
+            user=user,
+            name=name,
+            transaction_type=transaction_type
+        )
+        label.save()
+        _create_labels(children, transaction_type, label)
+
+
 def create_initial_labels(user_model, user, created, **kwargs):
-
-    def create_labels(tree, transaction_type, parent=None):
-        for name, children in tree:
-            label = Label(
-                parent=parent,
-                user=user,
-                name=name,
-                transaction_type=transaction_type
-            )
-            label.save()
-            create_labels(children, transaction_type, label)
-
-    create_labels(initial.withdrawal, transaction_type=Label.TRANSACTION_WITHDRAWAL)
+    if created:
+        _create_labels(initial.withdrawal, user, transaction_type=Label.TRANSACTION_WITHDRAWAL)
