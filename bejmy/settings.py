8bd21d118125e127ed58992f975aa30cc2b52bb7
Rafał Selewońko <rafal@selewonko.com>
2017-06-02 17:51:20 +0200

    Heroku app

--- bejmy/settings.py
+++ bejmy/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    # os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.ALLOWED_HOSTS#
