cf6cea6700be71f1700d8052f97399f3d8482040
Rafał Selewońko <rafal@selewonko.com>
2017-06-04 09:57:38 +0200

    Merge

--- bejmy/accounts/signals.py
+++ bejmy/accounts/signals.py
@@ -1,26 +1,43 @@
 from django.db.models import F
 
-from .models import Account
+from bejmy.accounts.models import Account
 from bejmy.transactions.models import Transaction
 
 
-def update_account_balance(sender, instance, **kwags):
+def _add_transaction(account, transaction):
+    Account.objects.filter(pk=account.pk) \
+       .update(balance=F('balance') + transaction.amount)
+
+
+def _subtract_transaction(account, transaction):
+    Account.objects.filter(pk=account.pk) \
+       .update(balance=F('balance') - transaction.amount)
+
+
+def _apply_transaction(transaction):
+    if transaction.source:
+        _subtract_transaction(transaction.source, transaction)
+
+    if transaction.destination:
+        _add_transaction(transaction.destination, transaction)
+
+
+def _remove_transaction(transaction):
+    if transaction.source:
+        _add_transaction(transaction.source, transaction)
+
+    if transaction.destination:
+        _subtract_transaction(transaction.destination, transaction)
+
+
+
+def update_account_balance(sender, instance, **kwargs):
     if instance.pk:
-        # its an existing transaction - undo account balance update
         old_instance = Transaction.objects.get(pk=instance.pk)
+        _remove_transaction(old_instance)
 
-        if old_instance.source:
-            Account.objects.filter(pk=old_instance.source.pk) \
-               .update(balance=F('balance') + old_instance.amount)
+    _apply_transaction(instance)
 
-        if old_instance.destination:
-            Account.objects.filter(pk=old_instance.destination.pk) \
-               .update(balance=F('balance') - old_instance.amount)
 
-    if instance.source:
-        Account.objects.filter(pk=instance.source.pk) \
-           .update(balance=F('balance') - instance.amount)
-
-    if instance.destination:
-        Account.objects.filter(pk=instance.destination.pk) \
-           .update(balance=F('balance') + instance.amount)
+def update_account_balance_on_delete(sender, instance, **kwargs):
+    _remove_transaction(instance)
